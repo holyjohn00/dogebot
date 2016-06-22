c7fe79860bc1394135ae661d0d1bf77d0b7ce71d
holyjohn00 <zgadiane@gmail.com>
2016-06-22 09:51:11 +0800

    Unable to install wolfram-alpha

--- bin/bot.js
+++ bin/bot.js
@@ -3,7 +3,7 @@
 
 var Bot = require('../lib/dogebot');
 
-var token = process.env.BOT_API_KEY;
+var token = "xoxb-49364486341-YCR4jVlx0hoT4rSNG5Ki67Jx";
 var dbPath = process.env.BOT_DB_PATH;
 var name = process.env.BOT_NAME;
 
